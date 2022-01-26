b4464e620e51f1479a8264cb2ae3e2839fe6776d
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-01-26 05:09:14 -0300

    Bkp arquivos

---
+++ JavaScritp/MatheusBatisti/10_objeto/js/script.js
@@ -0,0 +1,7 @@
+var pessoa = {
+    nome: "Danilo",
+    idade: 31,
+    profissao: "Front-end",
+};
+
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.idade} e sou um ${pessoa.profissao}`)
---
+++ JavaScritp/MatheusBatisti/5_number/js/script.js
@@ -0,0 +1,10 @@
+number = 1;
+
+console.log(typeof(number));
+
+
+var nome = "Danilo";
+var sobreNome = "Augusto";
+var nCompleto = `${nome} ${sobreNome}`;
+
+console.log(nCompleto);
