70f0e8ef0f7bde3c3bda6248c9fbb6b5f79d6907
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-02-03 06:18:08 -0300

    bkp arquivos

--- JavaScritp/MatheusBatisti/10_array copy/js/script.js
+++ JavaScritp/MatheusBatisti/10_array copy/js/script.js
@@ -1,7 +1,48 @@
-var lista = ["Banana","Maçã", "Pera", "Melancia", "Goiaba"];
+var lista = ['banana','maçã','uva','morngo'];
 
-var listaUl = document.createElement("ul");
+var body = document.getElementsByTagName('body');
 
-var body = document.getElementsByTagName("body");
+var ul = document.createElement('ul');
 
-// console.log(body);
+body[0].appendChild(ul);
+
+
+
+var listaUl = document.getElementsByTagName('ul');
+
+// listaUl[0].appendChild(listaFor);
+
+console.log(body[0]);
+
+
+for (var i = 0; i < lista.length; i++) {
+    var listaFor = document.createElement('li');
+    var textLi = document.createTextNode(lista[i]);
+
+    // listaFor.appendChild(textLi);
+
+    listaUl[0].appendChild(textLi);
+
+
+    console.log(listaFor);
+}
+
+
+
+// var lista = ['banana','maçã','uva','morngo'];
+
+// var body = document.getElementsByTagName('body');
+
+// var ul = document.createElement('ul');
+
+// body[0].appendChild(ul);
+
+// var li = document.createElement('li');
+
+// var listaUl = document.getElementsByTagName('ul');
+
+// listaUl[0].appendChild(li);
+
+// console.log(body[0]);
+
+
